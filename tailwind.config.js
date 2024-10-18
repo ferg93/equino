5bd34f074e9771ef2d71ca28b5eb6a1707c779cb
Fabiola Rodriguez <frodriguez@teravisiontech.com>
2024-10-18 03:50:24 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "templates/**/*.{html,js}",
+    "static/**/**/*.{js,css}",
+    // Add paths to other apps if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
